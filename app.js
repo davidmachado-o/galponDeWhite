af01a48077516c493e60bc91eac9bfa84650d393
davemachado-o <davidmachadodj@gmail.com>
2022-05-05 14:56:35 -0300

    mod port const

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
+const port = 3000;
+
 const publicPath = path.resolve(__dirname, "public");
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log("Running on port 3000");
 });
 
